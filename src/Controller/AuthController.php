a01f30902f4fb9a9c33c8777fb964940eea594ac
a.andreev <a.moskvin@rts-tender.ru>
2025-03-17 13:05:51 +0700

    #auth Авторизация пользователя, настройка конфигов

---
+++ src/Controller/AuthController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
+
+class AuthController extends AbstractController
+{
+    #[Route('/api/me', name: 'api_me', methods: ['GET'])]
+    public function meAction(#[CurrentUser] User $user): JsonResponse
+    {
+        return $this->json([
+            'email' => $user->getEmail(),
+            'roles' => $user->getRoles(),
+        ]);
+    }
+}
